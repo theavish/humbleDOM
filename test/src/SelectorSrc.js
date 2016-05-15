1412588ca47db179dd6dabc0489fbf39964ce8c4
jonkvix <jonkvix@gmail.com>
2016-05-15 13:36:51 -0700

    completed: tests for core _h element selector

--- test/src/SelectorSrc.js
+++ test/src/SelectorSrc.js
@@ -1,3 +1,14 @@
 var humbleDOM = function(selector) {
   return true;
 };
+
+var _h = function(selector) {
+	var elements = document.querySelectorAll(selector);
+	var elementArray = [];
+	for (var i = 0; i < elements.length; i++) {
+		var node = elements[i];
+		elementArray.push(node);
+	}
+	return elementArray;
+	// return Array.prototype.slice.call(elements);
+};
